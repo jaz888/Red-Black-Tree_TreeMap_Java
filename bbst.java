558fa6362f52e54a9e6a12cf626bef6174239894
Jieao Zhu <zhujieao@gmail.com>
2016-03-13 21:57:16 -0400

    initial bbst class which is the main entrance of the program

---
+++ bbst.java
@@ -0,0 +1,3 @@
+// this is the main entrance of program
+// need to support redirected input from a file "file-name" which specified in terminal
+// the file "file-name" contains the initial sorted list
