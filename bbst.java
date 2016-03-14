be7a52f41790ae71adb4743fa68473cc8ccbb3f0
Jieao Zhu <zhujieao@gmail.com>
2016-03-14 00:06:28 -0400

    Color definition

--- bbst.java
+++ bbst.java
@@ -1,3 +1,9 @@
-// this is the main entrance of program
+// this is the main entrance of the program
 // need to support redirected input from a file "file-name" which specified in terminal
 // the file "file-name" contains the initial sorted list
+import main.*;
+public class bbst{
+  public static void main(String[] args){
+    
+  }
+}
---
+++ main/TreeNode.java
@@ -0,0 +1,6 @@
+package main;
+
+public class TreeNode{
+  private Color color;
+  
+}
