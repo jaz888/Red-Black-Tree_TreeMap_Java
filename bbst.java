59f6c9365f12a0d186782fb4a07e8e6d388bb616
Jieao Zhu <zhujieao@gmail.com>
2016-03-16 18:40:45 -0400

    package location

--- bbst.java
+++ bbst.java
@@ -1,7 +1,4 @@
-// this is the main entrance of the program
-// need to support redirected input from a file "file-name" which specified in terminal
-// the file "file-name" contains the initial sorted list
-
+// test program for red black tree
 import com.jieao.*;
 import java.io.*;
 import java.util.*;
